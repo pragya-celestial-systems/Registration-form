b13c35317299ffeae84bbe9de2994a6c968fbee7
Celestial <celestial@CELSYSNCROOO036.local>
2024-10-10 18:20:37 +0530

    bug fixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const inputEl = document.querySelectorAll("input");
 const form = document.querySelector("#userdataForm");
 const gender = document.querySelectorAll(".gender");
-const username = document.querySelector("#username");
+const name = document.querySelector("#fullName");
 const number = document.querySelector("#number");
 const email = document.querySelector("#email");
 const qualification = document.querySelector("#qualification");
@@ -17,20 +17,28 @@ const tableContainer = document.querySelector('.table');
 const navLink = document.querySelectorAll('.link');
 const backdropContainer = document.querySelector('.backdrop');
 const modal = document.querySelector('#modal');
+const dateOfBirth = document.querySelector('#calender');
 
 let errorMessage, imageUrl, trashButtons, editButtons, viewButtons, isEditing = false;
 
 // --------------------------------------------------------
 
-function validateInput(username, email, number, qualification) {
+function validateInput(username, email, number, qualification, dob) {
   if (!username || username.trim() === "") {
-    return "Username can't be an empty string.";
-  } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+    return "Please enter your full name.";
+  }
+  else if (!email || email.length <= 0) {
+   return "Please enter email." 
+  }
+  else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
     return "Email is not valid.";
   } else if (number.length !== 10 || number < 0) {
     return "Number should contain 10 positive digits.";
   } else if (!qualification) {
     return "Please select qualification.";
+  }
+  else if (dob.length <= 0) {
+    return 'Please enter your date of birth.';
   }
   return "success";
 }
@@ -55,11 +63,12 @@ function validateAndUploadImage(d) {
 function addUserInTheTable(user, index) {
   const html = `<tr>
                 <td>${index + 1}</td>
-                <td>${user.username}</td>
-                <td>${user.email}</td>
-                <td>${user.gender}</td>
-                <td>${user.qualification}</td>
-                <td>${user.number}</td>
+                <td>${user.name || "N/A"}</td>
+                <td>${user.gender || "N/A"}</td>
+                <td>${user.email || "N/A"}</td>
+                <td>${user.dateOfBirth || "dd-mm-yyyy"}</td>
+                <td>${user.qualification || "N/A"}</td>
+                <td>${user.number || "N/A"}</td>
                 <td><i class="fa-regular fa-eye"></i><i class="fa-regular fa-pen-to-square"></i><i class="fa-solid fa-trash"></i></td>
             </tr>`;
 
@@ -67,15 +76,17 @@ function addUserInTheTable(user, index) {
 }
 
 function resetInput() {
-  username.value = "";
+  name.value = "";
   email.value = "";
   number.value = "";
+  calender.value = '';
 
   gender.forEach((btn) => (btn.checked = false));
-  console.log(qualification[qualification.selectedIndex]);
   image.value = "";
   imageContainer.removeAttribute("src");
   imageContainer.style.display = "none";
+  errorEl.textContent = '';
+  errorEl.style.opacity = 0;
 }
 
 function findUser(email) {
@@ -86,18 +97,36 @@ function findUser(email) {
   return users.find(user => user.email === email);
 }
 
-function createModalContent(user) {
+async function createModalContent(user) {
   const modalContent = `
      <div id="modalContent">
-        <p class="modal-username"><b>Username : </b>${user.username}</p>
+     <img src='${user.image}' alt="user-image" id="modalImg" />
+        <p class="modal-name"><b>Name : </b>${user.name}</p>
         <p class="modal-email"><b>Email : </b>${user.email}</p>
-        <p class="modal-gender"><b>Gender : </b>${user.gender}</p>
-        <p class="modal-qualification"><b>Qualication : </b>${user.qualification}</p>
+        <p class="modal-gender"><b>Gender : </b>${user.gender || 'N/A'}</p>
+        <p class="modal-dob"><b>Date of Birth : </b>${user.dateOfBirth || 'yyyy-mm-dd'}</p>
+        <p class="modal-qualification"><b>Qualification : </b>${user.qualification}</p>
         <p class="contact"><b>Contact : </b>${user.number}</p>
     </div>
   `;
 
+  // resetting the previous user data
+  modal.textContent = '';
+
   modal.insertAdjacentHTML('afterbegin', modalContent);
+}
+
+function setMaxDateToToday() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = today.getMonth() + 1;
+    const dd = today.getDate();
+
+    const formattedDate = `${yyyy}-${mm.toString().padStart(2, "0")}-${dd
+      .toString()
+      .padStart(2, "0")}`;
+
+    dateOfBirth.setAttribute("max", formattedDate);
 }
 
 // --------------------------------------------
@@ -131,24 +160,41 @@ profileImg.addEventListener("change", async (e) => {
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const validate = validateInput(
-    username.value,
+  const res = validateInput(
+    name.value,
     email.value,
     number.value,
-    qualification.options[qualification.selectedIndex].value
+    qualification.options[qualification.selectedIndex].value,
+    dateOfBirth.value,
   );
 
-  if (validate !== "success") {
+    if (res !== "success") {
+      errorEl.style.opacity = 1;
+      errorEl.textContent = res;
+      return;
+    }
+
+  let selectedGender;
+  
+  gender.forEach(gen => {
+    if (gen.checked) {
+      selectedGender = gen.value;
+    }
+  })
+
+  if (selectedGender === undefined) {
+    errorEl.textContent = 'Please select the gender.'
     errorEl.style.opacity = 1;
-    errorEl.textContent = validate;
     return;
   }
 
   const user = findUser(email.value);
-  if (user) {
-    errorEl.style.opacity = 1;
-    errorEl.textContent = 'User already exists.';
-    return;
+  if (!isEditing) {
+    if (user) {
+      errorEl.style.opacity = 1;
+      errorEl.textContent = 'User already exists.';
+      return;
+    }
   }
 
   errorEl.style.opacity = 0;
@@ -156,7 +202,6 @@ form.addEventListener("submit", async (e) => {
   let base64Image;
   try {
     base64Image = await validateAndUploadImage(imageUrl);
-    console.log(base64Image);
   } catch (err) {
     errorEl.textContent = "Error uploading the image.";
     errorEl.style.opacity = 1;
@@ -164,25 +209,26 @@ form.addEventListener("submit", async (e) => {
   }
 
   let userData = {
-    username: username.value,
+    name: name.value,
     number: number.value,
     email: email.value,
     qualification: qualification.options[qualification.selectedIndex].value,
     image: base64Image,
+    gender :  selectedGender,
+    dateOfBirth: dateOfBirth.value
   };
 
-  for (let i = 0; i < gender.length; i++) {
-    if (gender[i].checked) {
-      userData.gender = gender[i].value;
-      break;
-    }
-  }
+  submitBtn.textContent = 'Submit';
+  // TODO - find the user, if user already exists, update the details else create new user and save in the local storage
 
   const users = JSON.parse(localStorage.getItem("users")) || [];
   users.push(userData);
   localStorage.setItem("users", JSON.stringify(users));
+  console.log(userData);
 
   resetInput();
+  alert('form submitted successfully.')
+  window.location.reload();
 });
 
 // ------------------------------------------------
@@ -194,8 +240,10 @@ resetBtn.addEventListener("click", () => {
 // ------------------------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-  submitBtn.textContent = "Submit";
-  tableContainer.classList.add("active");
+  console.log('page reloaded');
+  setMaxDateToToday();
+
+  formContainer.classList.add("active");
   const users = JSON.parse(localStorage.getItem("users"));
 
   users.forEach((user, index) => {
@@ -211,24 +259,34 @@ window.addEventListener('DOMContentLoaded', () => {
   // if user clicks on the edit button
   editButtons.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      const emailAddress = btn.closest("tr").cells[2].textContent;
+      submitBtn.textContent = 'Update'
+      const emailAddress = btn.closest("tr").cells[3].textContent;
 
       const user = findUser(emailAddress);
 
       const confirm = window.confirm("Do you want to edit this entry?");
 
       if (!confirm) return;
-
+      isEditing = true;
       tableContainer.classList.remove("active");
       formContainer.classList.add("active");
 
-      console.log(user);
-      username.value = user.username;
+      name.value = user.name;
       email.value = user.email;
       number.value = user.number;
+      dateOfBirth.value = user.dateOfBirth;
+      qualification.value = user.qualification
 
-      isEditing = true;
-    });
+      if (user.gender.toLowerCase() === 'male') {
+        gender[0].checked = true;
+      }
+      else if (user.gender.toLowerCase() === "female") {
+        gender[1].checked = true;
+      } else {
+        user.gender[2].checked = true;
+      }
+
+      });
   });
 
   // add event listeners to all trash buttons
@@ -240,7 +298,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
       if (!confirm) return;
 
-      const emailAddress = btn.closest("tr").cells[2].textContent;
+      const emailAddress = btn.closest("tr").cells[3].textContent;
 
       const idx = users.findIndex((user) => user.email === emailAddress);
 
@@ -249,21 +307,15 @@ window.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem("users", JSON.stringify(users));
 
       window.location.reload();
-
-      // btn.parentElement.parentElement.remove()
-
-      // remove from the local storage
     });
-    // console.log(btn.parentElement.parentElement);
   });
 
   // show a modal if user clicks on the view button
   viewButtons.forEach((btn, index) => {
     btn.addEventListener('click', (e) => {
-      const emailAddress = e.target.closest('tr').cells[2].textContent;
+      const emailAddress = e.target.closest('tr').cells[3].textContent;
       const user = findUser(emailAddress);
-      console.log(user);
-
+      console.log(emailAddress, user);
       if (!user) {
         alert("Couldn't find user.");
         return;
@@ -272,7 +324,6 @@ window.addEventListener('DOMContentLoaded', () => {
       backdropContainer.classList.toggle('hidden-backdrop');
 
       createModalContent(user);
-
     })
   })
 })
@@ -283,7 +334,8 @@ backdropContainer.addEventListener('click', () => {
 
 navLink.forEach(link => {
   link.addEventListener('click', (e) => {
-    // link.classList.remove("active");
+    submitBtn.textContent = 'Submit';
+    resetInput()
     if (e.target.textContent === 'Table') {
       formContainer.classList.remove('active');
       tableContainer.classList.add('active');
